f513fa8bee8bc35ea6ebd9d5b1c0a576880cae73
ODonnchadha <danieldonahue1231@hotmail.com>
2020-08-24 15:31:37 -0500

    Value controller.

---
+++ app.api/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using app.api.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace app.api.Controllers
+{
+    [ApiController(), Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+        private readonly ILogger<ValuesController> logger;
+
+        public ValuesController(DataContext context, ILogger<ValuesController> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = context.Values.FirstOrDefault(
+                value => value.Id == id);
+            return Ok(value);
+        }
+
+        [HttpGet()]
+        public IActionResult GetValues()
+        {
+            var values = context.Values.ToList();
+            return Ok(values);
+        }
+    }
+}
