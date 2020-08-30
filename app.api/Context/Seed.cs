0ea62812e99151912122bbe27deb404a84e27382
ODonnchadha <danieldonahue1231@hotmail.com>
2020-08-30 15:37:47 -0500

    Seed user data via Program. Addition of Photo entity.

---
+++ app.api/Context/Seed.cs
@@ -0,0 +1,34 @@
+﻿using app.api.Entities;
+using app.api.Helpers.Auth;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace app.api.Context
+{
+    public class Seed
+    {
+        public static void SeedUserData(DataContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var userData = System.IO.File.ReadAllText("Context/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                foreach(var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
+                    PasswordHash.Create("password", out passwordHash, out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+
+                    context.Users.Add(user);
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
